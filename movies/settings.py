40f07e54ba40b7d5c920ebb1b256143f5f7503c4
NoahL <lauffern@gmail.com>
2022-08-29 13:44:04 -0300

    final

--- movies/settings.py
+++ movies/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from pickle import FALSE, TRUE
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-5swb6_gw9_tz417-c-t_y!=pho599o20_ja+lkp(l_ero-k3pi
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
--- movies/views.py
+++ movies/views.py
@@ -1,28 +1,38 @@
-from django.http import HttpResponse
+
+from django.http import Http404, HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from .models import Movie
 
-data = {
-  'movies': [
-    {
-      'id': 5,
-      'title': 'Jawz',
-      'year': 1696,
-    },
-    {
-      'id': 6,
-      'title': 'Sharknado',
-      'year': 2001,
-    },
-    {
-      'id': 7,
-      'title': 'The Land Before Time',
-      'year': 1999,
-    }
-  ]
-}
 
-def movies(req): 
-  return render(req, 'movies/movies.html', data)
+def movies(req):
+  data = Movie.objects.all()
+  return render(req, 'movies/movies.html', {'movies': data})
 
 def home(req): 
   return HttpResponse("HOME")
+
+def detail(req, id): 
+  data = Movie.objects.get(pk=id)
+  return render(req, 'movies/detail.html', {'movie': data})
+
+def add(req):
+  title = req.POST.get('title')
+  year = req.POST.get('year')
+
+  if title and year: 
+    movie = Movie(title=title, year=year)
+    movie.save()
+    return HttpResponseRedirect('/movies')
+
+  return render(req, 'movies/add.html')
+
+def delete(req, id): 
+  try: 
+    movie = Movie.objects.get(pk=id)
+    
+  except: 
+    raise Http404('Movie does not exist')
+
+  movie.delete()
+    
+  return HttpResponseRedirect('/movies')
